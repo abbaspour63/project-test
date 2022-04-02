a33edb54929d024ef56882d76131c20c678ce9c2
abbaspour63 <abbaspourab@gmail.com>
2022-04-02 20:03:55 +0430

    add laravelmix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
     <meta name="author" content="Rabie Elkheir">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap Core CSS -->
-
+s
     <!-- Owl Carousel Assets -->
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
           type="text/css" />
